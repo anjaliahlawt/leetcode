5d56634f7825acd2dece010a1610af3cad98c303
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-10-14 20:29:13 +0530

    added program

---
+++ Searching-program/sear3.java
@@ -0,0 +1,34 @@
+//278. First Bad Version
+public class sear3 
+{
+    public static void main(String[] args)
+    {
+        int n=10;
+        int bad=3;
+        int result=search(n,bad);
+        System.out.println(result);
+    }
+    static int search(int n,int bad)
+ {
+    int start=0;
+    int end=n;
+    int ans=0;
+    while(start<=end)
+    {
+        int mid=(start+end)/2;
+        if(mid==bad)
+        {
+            return mid;
+        }
+         if(mid<bad)
+        {
+            ans=mid;
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return ans;
+  }
+}
