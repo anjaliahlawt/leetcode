390af04ce95514a2e89529e1c8607baecdc085fa
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-05-17 10:28:08 +0530

    inital commit

--- array-program/arr5.java
+++ array-program/arr5.java
@@ -1,39 +1,56 @@
 //1470. Shuffle the Array
-import java.util.*;
-public class arr5 
-{
-        public static void main(String[] args) 
-        {
-            Scanner input=new Scanner(System.in);
-            System.out.println("enter the size of aaray");
-            int n=input.nextInt();
-            int[]nums=new int[2*n];
-             int[]ans=new int[2*n];
-            System.out.println("enter the value of aaray");
-            for(int i=0;i<2*n;i++)
-            {
-                nums[i]=input.nextInt();
-            }
-            System.out.println("the array is");
-            for(int i=0;i<2*n;i++)
-            {
-                System.out.print(nums[i] + " ");
-            }
-            System.out.println();
-            int j=0;
+// import java.util.*;
+// public class arr5 
+// {
+//         public static void main(String[] args) 
+//         {
+//             Scanner input=new Scanner(System.in);
+//             System.out.println("enter the size of aaray");
+//             int n=input.nextInt();
+//             int[]nums=new int[2*n];
+//              int[]ans=new int[2*n];
+//             System.out.println("enter the value of aaray");
+//             for(int i=0;i<2*n;i++)
+//             {
+//                 nums[i]=input.nextInt();
+//             }
+//             System.out.println("the array is");
+//             for(int i=0;i<2*n;i++)
+//             {
+//                 System.out.print(nums[i] + " ");
+//             }
+//             System.out.println();
+//             int j=0;
            
-             for( int i=0;i<2*n;i=i+2)
-             {
-                ans[i]=nums[j];
-                 ans[i+1]=nums[j+n];
-                 j++;
-             }
-              for(int i=0;i<2*n;i++)
-              {
-                System.out.print(ans[i] + " ");
-              }
-              input.close();
-        }
-}
+//              for( int i=0;i<2*n;i=i+2)
+//              {
+//                 ans[i]=nums[j];
+//                  ans[i+1]=nums[j+n];
+//                  j++;
+//              }
+//               for(int i=0;i<2*n;i++)
+//               {
+//                 System.out.print(ans[i] + " ");
+//               }
+//               input.close();
+//         }
+// }
     
-
+import java.util.*;
+public class arr5
+{
+    public static void main(String[] args) 
+    {
+        int []arr={2,5,1,3,4,7};
+        int n =3;
+        int []ans=new int[2*n];
+        int j=0;
+        for(int i=0;i<2*n;i=i+2)
+        {
+            ans[i]=arr[j];
+             ans[i+1]=arr[j+n];
+             j++;
+        }
+         System.out.println(Arrays.toString(ans));  
+     }
+}
--- array-program/arr7.java
+++ array-program/arr7.java
@@ -1,39 +1,60 @@
-//1512. Number of Good Pairs
-import java.util.*;
-public class arr7 
-{
-        public static void main(String[] args) 
-        {
-            System.out.println("enter the size of aaray");
-            Scanner input=new Scanner(System.in);
-            int n=input.nextInt();
-            int[]nums=new int[n];
-            System.out.println("enter the value of aaray");
-            for(int i=0;i<n;i++)
-            {
-                nums[i]=input.nextInt();
-            }
-            System.out.println("the aaray is");
-            for(int i=0;i<n;i++)
-            {
-               System.out.println(nums[i]);
-            }
-            int count=0;
-            for(int i=0;i<n;i++)
-            {
-                for(int j=i+1;j<n;j++)
-                {
-                    if(nums[i]==nums[j])
-                    {
-                      count++;
-                    }
+// //1512. Number of Good Pairs
+
+// import java.util.*;
+// public class arr7 
+// {
+//         public static void main(String[] args) 
+//         {
+//             System.out.println("enter the size of aaray");
+//             Scanner input=new Scanner(System.in);
+//             int n=input.nextInt();
+//             int[]nums=new int[n];
+//             System.out.println("enter the value of aaray");
+//             for(int i=0;i<n;i++)
+//             {
+//                 nums[i]=input.nextInt();
+//             }
+//             System.out.println("the aaray is");
+//             for(int i=0;i<n;i++)
+//             {
+//                System.out.println(nums[i]);
+//             }
+//             int count=0;
+//             for(int i=0;i<n;i++)
+//             {
+//                 for(int j=i+1;j<n;j++)
+//                 {
+//                     if(nums[i]==nums[j])
+//                     {
+//                       count++;
+//                     }
                     
-                }
+//                 }
                
-            }
-            input.close();
-            System.out.println("the output is"+" "+count);
-        }
- }
+//             }
+//             input.close();
+//             System.out.println("the output is"+" "+count);
+//         }
+//  }
     
 
+public class arr7
+{
+    public static void main(String[] args) 
+    {
+        int[] nums = {1,1,1,1};
+        int n=nums.length;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    count=count+1;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
