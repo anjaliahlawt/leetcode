1cb07e3fda65a1abea1f766bbbb40652547bf80e
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-05-21 15:16:37 +0530

    inital commit

--- Searching-program/sear4.java
+++ Searching-program/sear4.java
@@ -1,14 +1,18 @@
 //167. Two Sum II - Input Array Is Sorted
+
+// using linear search
+
+/* 
 import java.util.Arrays;
 
 public class sear4
 {
     public static void main(String[] args) 
     {
-        int []arr={-1,0};
+        int []arr={2,3,4};
         
         int n =arr.length;
-        int target=-1;
+        int target=6;
       int[]ans= sum(arr,target,n);
       System.out.println(Arrays.toString(ans));
     
@@ -22,16 +26,59 @@ public class sear4
             {
                 if(arr[i]+arr[j]==target)
                 {
-                    ans[0]=i;
-                    ans[1]=j;
+                    ans[0]=i+1;
+                    ans[1]=j+1;
                 }
             }
         }
-        return ans;
+        return ans ;
         
     }
 }
 
+ */
+import java .util.*;
+
+public class sear4
+{
+    public static void main(String[] args) 
+    {
+        int[]arr={2,7,11,15};
+        int target=9;
+     int []ans=sum(arr,target);
+        System.out.println(Arrays.toString(ans));
+        
+    }
+    static int [] sum(int[]arr,int target){
+        int[] ans = new int[2];
+        
+        int start=0;
+        int end=arr.length-1;
+        while(start<end)
+        {
+          int sum=arr[start]+arr[end];
+          if(sum==target)
+          {
+            ans[0]=start+1;
+            ans[1]=end+1;
+            return ans;
+            
+          }
+          if(target>sum)
+          {
+            start++;
+          }
+          if(target<sum)
+          {
+            end--;
+          }
+        }
+        return ans;
+      
+
+        
+    }
+}
 
 
 
