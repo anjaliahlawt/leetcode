bbacc91f39bd63b752d417108fcbbb9cb6b41f76
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-05-23 10:19:58 +0530

    inital commit

---
+++ Searching-program/sear6.java
@@ -0,0 +1,16 @@
+public class sear6
+{
+   public static void main(String[] args) 
+   {
+     int n=5;
+     int i=1;
+     while(n>0)
+     {
+        i++;
+        n=n-i;
+        
+     }
+     System.out.println(i-1);
+        }
+     }
+   
